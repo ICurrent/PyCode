61b271d96a0035bf6727457af71e3e88e72a468c
current <ogunnuel@gmail.com>
2022-08-18 08:17:17 -0700

    initial commit

---
+++ 00_GeneralCode/pythagoras.py
@@ -0,0 +1,6 @@
+# def pythagoras(opposite, adjcent, hypotenus):
+#     try:
+#         if opposite == str('?'):
+#             return("Opposite = " + str(((hyp))))
+
+from 01_General_Code.class_four import
